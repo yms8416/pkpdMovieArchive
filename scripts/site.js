35a6cf632d84b3b87740799b294c4c117ad72a06
Can PERK <bayms8416@gmail.com>
2018-04-21 14:01:47 +0300

    sinema arşivi sitesi başlangıç

---
+++ scripts/site.js
@@ -0,0 +1,8 @@
+﻿window.onload = function () {
+    var content = document.getElementById("content");
+    var leftMenu = document.getElementsByClassName("leftMenu");
+    var pageContent = document.getElementsByClassName("pageContent");
+    content.style.height = (document.documentElement.clientHeight - 100) + "px";
+    leftMenu[0].style.height = content.style.height;
+    pageContent[0].style.height = content.style.height;
+}
